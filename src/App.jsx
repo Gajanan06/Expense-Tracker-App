8feb791ab3918e9c3697f53a2bd43c1d5e9002ec
Gajanan Nilajkar <gajanannilajkar06@gmail.com>
2026-05-09 13:47:16 +0530

    Build expense form with react state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import CurrencyConverter from "./components/CurrencyConverter";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
@@ -5,19 +6,30 @@ import SummaryPanel from "./components/SummayPanel";
 
 function App() {
 
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpense = (newExpense) => {
+    setExpenses([...expenses,newExpense]);
+  };
+
   return (
-    <>
-    <div>
-      <h2 className="text-3xl text-center font-bold underline">Expense Tracker</h2>
-    </div>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-      <ExpenseForm />
-      <SummaryPanel />
-      <ExpenseList />
-      <CurrencyConverter />
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="max-w-6xl mx-auto">
+         <h2 className="text-4xl font-bold text-center text-green-600 mb-8">Expense Tracker</h2>
       </div>
 
-    </>
+     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+       <ExpenseForm addExpense={addExpense}/>
+
+       <SummaryPanel />
+
+       <ExpenseList />
+
+       <CurrencyConverter />
+
+       </div>
+    </div>
   )
 }
 
--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,7 +1,81 @@
-function ExpenseForm() {
+import { useState } from "react";
+
+function ExpenseForm(addExpense) {
+
+    const [title, setTitle] = useState("");
+    const [amount, setAmount] = useState("");
+    const [category, setCategory] = useState("Food");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!title.trim() || !amount) {
+            alert("Please fill the fields");
+            return;
+        }
+
+        const newExpense = {
+            id: Date.now(),
+            title: title.trim(),
+            amount: Number(amount),
+            category,
+        };
+
+        console.log(newExpense)
+        addExpense(newExpense);
+
+        setTitle("");
+        setAmount("");
+        setCategory("Food");
+    };
+
     return (
-        <div className="bg-white p-5 rounded-xl">
-            <h3 className="text-2xl mb-4">Add new Expense</h3>
+        <div className="bg-white p-6 rounded-xl shadow-md">
+            <h3 className="text-2xl font-semibold mb-5">Add new Expense</h3>
+
+            <form onSubmit={handleSubmit} className="space-y-4">
+
+                <div>
+                    <label className="block md-2 font-medium">Expense Name</label>
+                    <input required 
+                       type="text"
+                       placeholder="Enter Expense Name"
+                       value={title}
+                       onChange={(e) => setTitle(e.target.value)}
+                        className="w-full border border-gray-300 rounded-lg p-3 outline-none focus:border-green-500"/>
+                </div>
+
+                <div>
+                    <label className="block md-2 font-medium">Amount</label>
+                    <input required 
+                       type="number"
+                       min="1"
+                       placeholder="Enter Amount"
+                       value={amount}
+                       onChange={(e) => setAmount(e.target.value)}
+                       className="w-full border border-gray-300 rounded-lg p-3 outline-none focus:border-green-500"/>
+                </div>
+
+                <div>
+                    <label className="block md-2 font-medium">Category</label>
+                    <select 
+                       value={category}
+                       onChange={(e) => setCategory(e.target.value)}
+                       className="w-full border border-gray-300 rounded-lg p-3 outline-none focus:border-green-500">
+
+                       <option>Food</option>
+                       <option>Travel</option>
+                       <option>Marketing</option>
+                       <option>Utilities</option>
+                       <option>Other</option>
+                    </select>
+                </div>
+
+                <button
+                    type="submit"
+                    className="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-lg transition duration-300">
+                    Add Expense</button> 
+            </form>
         </div>
     );
 }
